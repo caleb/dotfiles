231d2cdc958e3432aa960421349f200119e1ef3b
Caleb Land <caleb@land.fm>
2014-11-05 12:41:37 -0500

    set up chruby

--- zprezto/modules/ruby/init.zsh
+++ zprezto/modules/ruby/init.zsh
@@ -30,6 +30,7 @@ elif (( $+commands[chruby-exec] )); then
   source "${commands[chruby-exec]:h:h}/share/chruby/chruby.sh"
   if zstyle -t ':prezto:module:ruby:chruby' auto-switch; then
     source "${commands[chruby-exec]:h:h}/share/chruby/auto.sh"
+    chruby_auto
   fi
 
 # Prepend local gems bin directories to PATH.
@@ -64,9 +65,4 @@ if (( $+commands[bundle] )); then
     && print vendor/assets >>! .gitignore \
     && print vendor/bundle >>! .gitignore \
     && print vendor/cache  >>! .gitignore'
-fi
-
-# Zeus
-if (( $+commands[zeus] )); then
-  alias z='zeus'
 fi
