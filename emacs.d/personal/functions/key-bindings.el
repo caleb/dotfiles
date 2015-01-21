8e85c17602200454910a7b0c28042c2b1d265acf
Caleb Land <caleb@land.fm>
2015-01-21 02:46:01 -0500

    Only keys that are single characters will be prefixed with C-

--- emacs.d/personal/functions/key-bindings.el
+++ emacs.d/personal/functions/key-bindings.el
@@ -2,7 +2,9 @@
 (defun personal/bind-key (prefix keys binding)
   "Binds a command with a prefix to a non-controlled and controlled version this helps with god-mode"
   (let* ((lst (split-string keys " "))
-         (ctrl-keys (mapconcat (lambda (key) (concat "C-" key)) lst " ")))
+         (ctrl-keys (mapconcat (lambda (key) (if (= (length key) 1)
+                                                 (concat "C-" key)
+                                               key)) lst " ")))
     (global-set-key (kbd (concat prefix " " keys)) binding)
     (global-set-key (kbd (concat prefix " " ctrl-keys)) binding)
-   ))
+    ))
