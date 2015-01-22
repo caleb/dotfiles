a06e1566ecfe6be9761bc7f3ef3306514147fa6b
Caleb Land <caleb@land.fm>
2015-01-21 19:10:28 -0500

    Add bindings that convert Capital letters to meta key combos
    this helps with god-mode, which in terminal mode can't distinguish
    between C-a and C-A... this means we can specify capital variants of
    commands and those will be turned into an additional binding to a meta variant
    
    e.g.
    
    C-l a b C
    
    will bind to C-l a b C
                 C-l C-a C-b C-C
                 C-l C-a C-b M-c
    
    this means in god-mode we can activate the last form by typing "l a b g
    c" because typing "g" in god mode prepends meta to the next character

--- emacs.d/personal/hooks/after-init.el
+++ emacs.d/personal/hooks/after-init.el
@@ -3,29 +3,25 @@
 ;; frame and window management
 (personal/bind-key "C-l" "f c" 'make-frame-command)
 (personal/bind-key "C-l" "f k" 'delete-frame)
-(personal/bind-key "C-l" "f f" (lambda ()
-                                  (interactive)
-                                  (other-frame -1)))
 (personal/bind-key "C-l" "f F" (lambda ()
-                                  (interactive)
-                                  (other-frame 1)))
+                                 (interactive)
+                                 (other-frame 1)))
+(personal/bind-key "C-l" "f f" (lambda ()
+                                 (interactive)
+                                 (other-frame -1)))
 
 (personal/bind-key "C-l" "f s" 'split-window-vertically)
 (personal/bind-key "C-l" "f v" 'split-window-horizontally)
-(personal/bind-key "C-l" "f q" 'delete-window)
 (personal/bind-key "C-l" "f Q" 'kill-buffer-and-window)
-(personal/bind-key "C-l" "f M-q" 'kill-buffer-and-window)
+(personal/bind-key "C-l" "f q" 'delete-window)
+
 (personal/bind-key "C-l" "f 1" 'delete-other-windows)
-(personal/bind-key "C-l" "f o" (lambda ()
-                                 (interactive)
-                                 (other-window -1)))
 (personal/bind-key "C-l" "f O" (lambda ()
                                  (interactive)
                                  (other-window 1)))
-
-(personal/bind-key "C-l" "f M-o" (lambda ()
-                                   (interactive)
-                                   (other-window 1)))
+(personal/bind-key "C-l" "f o" (lambda ()
+                                 (interactive)
+                                 (other-window -1)))
 
 (global-set-key (kbd "C-M-n") 'personal/other-window-next-line)
 (global-set-key (kbd "C-M-p") 'personal/other-window-previous-line)
@@ -50,22 +46,22 @@
 
 (personal/bind-key "C-l" "/" 'personal/comment-or-uncomment-region-or-line)
 
-(personal/bind-key "C-l" "l"   'personal/newline-below)
 (personal/bind-key "C-l" "L"   'personal/newline-above)
+(personal/bind-key "C-l" "l"   'personal/newline-below)
 (personal/bind-key "C-l" "RET" 'personal/newline-between)
 
 (personal/bind-key "C-l" "#" 'personal/insert-header-comment)
 
-(personal/bind-key "C-l" "r"   'replace-string)
 (personal/bind-key "C-l" "R"   'replace-regexp)
+(personal/bind-key "C-l" "r"   'replace-string)
 (personal/bind-key "C-l" "o e" 're-builder)
 
 
 (personal/bind-key "C-l" "-" (lambda ()
-                                (interactive)
-                                (if (buffer-file-name)
-                                    (dired (file-name-directory (buffer-file-name)))
-                                    (dired default-directory))))
+                               (interactive)
+                               (if (buffer-file-name)
+                                   (dired (file-name-directory (buffer-file-name)))
+                                 (dired default-directory))))
 
 (personal/bind-key "C-l" "." 'repeat)
 
@@ -78,8 +74,8 @@
 
 (personal/bind-key "C-l" "s" 'idomenu)
 
-(personal/bind-key "C-l" "a" 'ag-project-regexp)
 (personal/bind-key "C-l" "A" 'ag-project-dired-regexp)
+(personal/bind-key "C-l" "a" 'ag-project-regexp)
 
 (personal/bind-key "C-l" "H" 'personal/htmlify-buffer)
 (personal/bind-key "C-l" "h" 'personal/htmlify-buffer-light)
@@ -98,8 +94,8 @@
 
 (personal/bind-key "C-l" "b l" 'nlinum-mode)
 
-(personal/bind-key "C-l" "o r" 'inf-ruby)
 (personal/bind-key "C-l" "o R" 'personal/rails-console)
+(personal/bind-key "C-l" "o r" 'inf-ruby)
 (personal/bind-key "C-l" "o d" 'personal/rails-dbconsole)
 (personal/bind-key "C-l" "o s" 'personal/open-shell-in-ansi-term)
 (personal/bind-key "C-l" "o m" 'man)
@@ -109,6 +105,9 @@
 
 ;; Make some common actions easier to activate with god-mode
 (global-set-key (kbd "C-x C-1") #'delete-other-windows)
+(global-set-key (kbd "C-x C-b") #'helm-buffers-list)
+(global-set-key (kbd "C-x C-k") #'ido-kill-buffer)
+
 
 ;; Allow - to go up a directory in dired (like vim)
 (define-key dired-mode-map "-" 'dired-up-directory)
