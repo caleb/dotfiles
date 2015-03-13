27f742a3d76aa2d3c6bcfeb34f56e239f66dfd8a
Caleb Land <caleb@land.fm>
2015-03-13 11:47:03 -0400

    Experiment with using emacs mode in multiple cursors mode

--- emacs.d/personal/settings/multiple-cursors.el
+++ emacs.d/personal/settings/multiple-cursors.el
@@ -52,6 +52,68 @@
 (add-hook 'multiple-cursors-mode-disabled-hook (lambda ()
                                                  (setq evil-move-cursor-back t)))
 
+
+;;
+;; Multiple cursors evil compat (use emacs mode during mc)
+;;
+(defun personal/evil-visual-or-normal-p ()
+  "True if evil mode is enabled, and we are in normal or visual mode."
+  (and (bound-and-true-p evil-mode)
+       (not (memq evil-state '(insert emacs)))))
+
+(defvar personal/evil-prev-state nil)
+(defvar personal/mark-was-active nil)
+
+(defun personal/switch-to-emacs-state ()
+  (when (personal/evil-visual-or-normal-p)
+
+    (setq personal/evil-prev-state evil-state)
+
+    (when (region-active-p)
+      (setq personal/mark-was-active t))
+
+    (let ((mark-before (mark))
+          (point-before (point)))
+
+      (evil-emacs-state 1)
+
+      (when (or personal/mark-was-active (region-active-p))
+        (goto-char point-before)
+        (set-mark mark-before)))))
+
+(defun personal/back-to-previous-state ()
+  (when personal/evil-prev-state
+    (unwind-protect
+        (case personal/evil-prev-state
+          ((normal visual) (evil-force-normal-state))
+          (t (message "Don't know how to handle previous state: %S"
+                      personal/evil-prev-state)))
+      (setq personal/evil-prev-state nil)
+      (setq personal/mark-was-active nil))))
+
+(add-hook 'multiple-cursors-mode-enabled-hook
+          'personal/switch-to-emacs-state)
+(add-hook 'multiple-cursors-mode-disabled-hook
+          'personal/back-to-previous-state)
+
+(defun personal/rectangular-switch-state ()
+  (if rectangular-region-mode
+      (personal/switch-to-emacs-state)
+    (setq personal/evil-prev-state nil)))
+
+;; When running edit-lines, point will return (position + 1) as a
+;; result of how evil deals with regions
+(defadvice mc/edit-lines (before change-point-by-1 activate)
+  (when (user-utils/evil-visual-or-normal-p)
+    (if (> (point) (mark))
+        (goto-char (1- (point)))
+      (push-mark (1- (mark))))))
+
+(add-hook 'rectangular-region-mode-hook 'personal/rectangular-switch-state)
+
+;;
+;; Configure some default methods to execute for one or multiple cursors
+;;
 (mapc (lambda (cmd)
         (add-to-list 'mc/cmds-to-run-for-all cmd))
       '(
