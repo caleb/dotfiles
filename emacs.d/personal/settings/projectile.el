5367ec9dca4a4b5d5b90b7f00642471c04cb2606
Caleb Land <caleb@land.fm>
2015-06-14 20:49:59 -0400

    add emacs boilerplate

--- emacs.d/personal/settings/projectile.el
+++ emacs.d/personal/settings/projectile.el
@@ -1,2 +1,13 @@
+;;; projectile.el --- Settings for projectile
+
+;;; Commentary:
+
+;; Settings for projectile
+
+;;; Code:
+
+;; Enable projectile-rails
 (setq projectile-rails-keymap-prefix (kbd "C-l p r"))
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
+
+;;; projectile.el ends here
