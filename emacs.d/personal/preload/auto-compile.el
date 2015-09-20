e4a0332aa7fd6a9cbd466e95c3c320452830b7f6
Caleb Land <caleb@land.fm>
2015-09-19 22:56:01 -0400

    Remove auto-compile and dependencies

--- emacs.d/personal/preload/auto-compile.el
+++
@@ -1,5 +0,0 @@
-;;; auto-compile.el --- auto-compile emacs lisp      -*- no-byte-compile: t -*-
-;;(require 'auto-compile)
-;; This breaks projectile
-;;(auto-compile-on-load-mode 1)
-;;(auto-compile-on-save-mode 1)
