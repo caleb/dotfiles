ca53d357157ef062a31be5b026c37b8bfff85205
Caleb Land <caleb@land.fm>
2015-10-11 21:22:33 -0400

    add comments

--- emacs.d/personal/settings/rake.el
+++ emacs.d/personal/settings/rake.el
@@ -1,2 +1,12 @@
+;;; rake.el --- Settings for rake
+
+;;; Commentary:
+
+;; Settings for rake
+
+;;; Code:
+
 (eval-after-load 'projectile
   '(setq rake-completion-system projectile-completion-system))
+
+;;; rake.el ends here
