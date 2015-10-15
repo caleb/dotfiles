3d98dcf45caabd7d887bb2c7123134e4cf289c65
Caleb Land <caleb@land.fm>
2015-10-15 10:09:09 -0400

    fix theme of hlinum

--- emacs.d/personal/settings/linum.el
+++ emacs.d/personal/settings/linum.el
@@ -14,6 +14,6 @@
 
 ;; Set the linum background color to be the same as the background in gruvbox
 (set-face-attribute 'linum nil :background (if (display-graphic-p) "#282828" "color-235"))
-(set-face-attribute 'linum-highlight-face nil :background (if (display-graphic-p) "#282828" "color-235"))
+(set-face-attribute 'linum-highlight-face nil :background (if (display-graphic-p) "#3c3836" "color-237") :foreground (if (display-graphic-p) "#fabd2f" "color-214"))
 
 ;;; linum.el ends here
