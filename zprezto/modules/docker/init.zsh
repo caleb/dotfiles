78c4a068654343770f0db8f0a7a10807b1110e5a
Caleb Land <caleb@land.fm>
2016-04-13 13:12:53 -0400

    dont use dinghy/boot2docker if the Docker.app is installed

--- zprezto/modules/docker/init.zsh
+++ zprezto/modules/docker/init.zsh
@@ -9,41 +9,44 @@ if (( ! $+commands[docker] )); then
   return 1
 fi
 
-if (( $+commands[boot2docker] )); then
-  alias b2d="boot2docker"
-  alias b2di="\$(boot2docker shellinit 2>/dev/null)"
+# If the OS X Docker app is installed then don't set up dingy/boot2docker
+if [[ ! -d /Applications/Docker.app ]]; then
+  if (( $+commands[boot2docker] )); then
+    alias b2d="boot2docker"
+    alias b2di="\$(boot2docker shellinit 2>/dev/null)"
 
-  # Start boot2docker if it isn't running
-  boot2docker_status=$(boot2docker status)
+    # Start boot2docker if it isn't running
+    boot2docker_status=$(boot2docker status)
 
-  if [[ "${boot2docker_status}" = "running" ]]; then
-    $(boot2docker shellinit 2>/dev/null)
+    if [[ "${boot2docker_status}" = "running" ]]; then
+      $(boot2docker shellinit 2>/dev/null)
+    fi
+
+    unset boot2docker_status
   fi
 
-  unset boot2docker_status
-fi
+  if (( $+commands[docker-machine] )); then
+    alias dm="docker-machine"
 
-if (( $+commands[docker-machine] )); then
-  alias dm="docker-machine"
+    function {
+      setopt LOCAL_OPTIONS RE_MATCH_PCRE
 
-  function {
-    setopt LOCAL_OPTIONS RE_MATCH_PCRE
+      if (( $+commands[dinghy] )); then
+        $(dinghy shellinit)
+      else
+        local dm_status="$(docker-machine ls)"
+        local b2d_dm="$(echo "${dm_status}" | grep '^boot2docker\b')"
 
-    if (( $+commands[dinghy] )); then
-      $(dinghy shellinit)
-    else
-      local dm_status="$(docker-machine ls)"
-      local b2d_dm="$(echo "${dm_status}" | grep '^boot2docker\b')"
+        if [[  "${b2d_dm}" =~ ^boot2docker ]]; then
+          alias dmi="eval \"\$(docker-machine env boot2docker)\""
 
-      if [[  "${b2d_dm}" =~ ^boot2docker ]]; then
-        alias dmi="eval \"\$(docker-machine env boot2docker)\""
-
-        if [[ "${b2d_dm:l}" =~ \\brunning\\b ]]; then
+          if [[ "${b2d_dm:l}" =~ \\brunning\\b ]]; then
             eval "$(docker-machine env boot2docker)"
+          fi
         fi
       fi
-    fi
-  }
+    }
+  fi
 fi
 
 if (( $+commands[docker-compose] )); then
