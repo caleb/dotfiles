62ab86c0ade374af07b0e0eee647cee84b74742f
Caleb Land <caleb@land.fm>
2020-01-12 15:40:09 -0500

    Updates

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,4 @@
 {:user
- {:plugins      [[venantius/ultra "0.5.1"]
-                 [refactor-nrepl "2.2.0"]
-                 [cider/cider-nrepl "0.14.0"]]
-  :dependencies [[org.clojure/tools.namespace "0.2.11"]]
+ {:plugins      []
+  :dependencies []
   }}
